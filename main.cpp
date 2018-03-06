45e86243c694b23f454d71ae07ed1c374121a0aa
Du Park <dupark3@gmail.com>
2018-03-06 06:25:20 -0500

    Main function reads input and stores in vector of class objects

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm> // sort, max
+#include <cstddef> // size_t
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,6 +10,14 @@ int main() {
     std::cout << "Enter each student's name, midterm grade, final grade, and homework grades: " << std::endl;
     Student_info record;
     std::vector<Student_info> students;
+    std::string s;
+    size_t maxLen = 0;
+
+    // read and store
+    while(record.read(std::cout)){
+        maxLen = std::max(maxLen, record.name().size());
+        students.push_back(record);
+    }
 
 
     return 0;
