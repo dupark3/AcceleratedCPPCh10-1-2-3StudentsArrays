27a5ac2b00d6cc6a0ee0f9f6fe127b57687ed1e0
Du Park <dupark3@gmail.com>
2018-03-07 09:30:32 -0500

    Commented added for Exercise 10-1 and 10-2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+Exercise 10-1 Rewrite the student-grading program from page 166 to generate letter grades. 
+
+Exercise 10-2 Rewirte the median function from page 140 so that we can call it witheither a vector or a built-in array. The function should allow contaiers or any arithmetic type. 
+
+*/
+
 #include <algorithm> // sort, max
 #include <cstddef> // size_t
 #include <iostream>
@@ -7,7 +14,7 @@
 #include "Student_info.h"
 
 int main() {
-    std::cout << "Enter each student's name, midterm grade, and final grade: " << std::endl;
+    std::cout << "Enter each student's name, midterm grade, final grade, and homework grades: " << std::endl;
     Student_info record;
     std::vector<Student_info> students;
     size_t maxLen = 0;
